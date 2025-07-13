e589aa8df65f616ed4d32cd7313de3c7e4406593
MelisaBaturay <melisabaturay67@gmail.com>
2025-07-13 16:03:36 +0300

    admin login page added

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kampusum.Controllers
+{
+    public class AdminController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string pass)
+        {
+            if (username == "admin" && pass == "melisa")
+            {
+                return RedirectToAction("Panel");
+            }
+
+            ViewBag.LoginError = "Kullanıcı adı veya şifre hatalı.";
+            return View();
+        }
+        public IActionResult Panel()
+        {
+            return View();
+        }
+    }
+}
