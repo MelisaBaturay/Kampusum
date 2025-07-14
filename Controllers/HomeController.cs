9fbf94f3d8bf2b7cfe7d0a0bf580070f2c7c8a68
MelisaBaturay <melisabaturay67@gmail.com>
2025-07-14 20:19:26 +0300

    admin news adding compleated

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Kampusum.Models;
 
 namespace Kampusum.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly Context _context;
+        public HomeController(Context context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult Starter()
         {
-            return View();
+            var newsList = _context.Newss.ToList();
+            return View(newsList);
         }
         public IActionResult StudentLife()
         {
--- Controllers/NewsController.cs
+++ Controllers/NewsController.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Kampusum.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Kampusum.Controllers
 {
     public class NewsController : Controller
     {
+        private readonly Context _context;
+
+        public NewsController(Context context)
+        {
+            _context = context;
+        }
+        [Authorize]
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(News news)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Newss.Add(news);
+                _context.SaveChanges();
+                return RedirectToAction("Create");
+            }
+
+            return View(news);
         }
     }
 }
